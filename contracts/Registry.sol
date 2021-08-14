0ef43352e601567f23abefcc4c8c7ce1778327cc
Richie Humphrey <eihcir0@gmail.com>
2021-08-14 14:19:57 -0700

    feat(registry): adds Events

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -4,13 +4,19 @@ pragma solidity >=0.8.4;
 contract Registry {
     mapping(string => address) public claimedNames;
 
+    event NameClaimed(address _by, string _name);
+
+    event NameReleased(address _by, string _name);
+
     function claimName(string memory _name) public {
         require(claimedNames[_name] == address(0), "Name already claimed");
         claimedNames[_name] = msg.sender;
+        emit NameClaimed(msg.sender, _name);
     }
 
     function releaseName(string memory _name) public {
         require(claimedNames[_name] == msg.sender, "Unauthorized");
         claimedNames[_name] = address(0);
+        emit NameReleased(msg.sender, _name);
     }
 }
