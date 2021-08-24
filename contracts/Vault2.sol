bedbeb590ced1ca85072287605bd5732765c0fa9
Richie Humphrey <eihcir0@gmail.com>
2021-08-23 17:28:42 -0700

    feat(vault2): address code review comments

--- contracts/Vault2.sol
+++ contracts/Vault2.sol
@@ -9,7 +9,7 @@ import "@yield-protocol/utils-v2/contracts/token/IERC20.sol";
 // Issues TooliganVaultToken ERC20 tokens for deposits
 contract Vault2 is ERC20("TooliganVaultToken", "TVT", 18) {
     //@notice This vault only accepts one type of token which is passed in at deploy
-    IERC20 private _token;
+    IERC20 public _token;
 
     event Deposit(uint256 wad);
     event Withdraw(uint256 wad);
@@ -18,12 +18,19 @@ contract Vault2 is ERC20("TooliganVaultToken", "TVT", 18) {
         _token = token;
     }
 
+    // @notice Function to deposit funds into the vault by means of
+    // transfering in tokens and minting TVT to the depositor
+    // @param wad Amount being deposited
     function deposit(uint256 wad) external {
         _mint(msg.sender, wad);
         _token.transferFrom(msg.sender, address(this), wad);
         emit Deposit(wad);
     }
 
+    // @notice Function to withdraw funds from the vault by means of
+    // transfering in TVT tokens and burning them, then transferring out
+    // the ERC20 tokens
+    // @param wad Amount being withdrawn
     function withdraw(uint256 wad) external {
         _burn(msg.sender, wad);
         _token.approve(msg.sender, wad);
